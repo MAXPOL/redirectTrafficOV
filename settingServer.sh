dc013986dcba620e26b712e20c93999148a17ecb
MAXPOL <maxrol32@gmail.com>
2021-10-06 22:20:35 +0300

    Create settingServer.sh

---
+++ settingServer.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+echo "Enter WAN ip_address you server: "
+read wanip
+
+echo "Enter password for CA cert: "
+read passwordcacert
+
+echo "You need password for cert ? (y/n)"
+read passcert
+
+if [[ $passcert = 'y' ]] 
+then 
+	echo "Enter password for server cert: "
+	read servercert
+
+	echo "Enter password for client cert: "
+	read clientcert
+fi
+
+if [[ $passcert = 'n' ]] 
+then 
+	continue;
+fi
+
+if [[ $passcert != 'n' && passcert != 'y' ]] 
+then 
+	clear
+	echo "NOT CORRECT DATA"
+	exit 0
+fi
+
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+systemctl enable firewalld
+systemctl start firewalld
+firewall-cmd --permanent --add-port=443/udp
+firewall-cmd --permanent --zone=public --add-port=80/tcp
+firewall-cmd --reload
+
+yum install epel-release nano wget httpd -y
+yum install openvpn easy-rsa dnsmasq -y
+
+cd /usr/share/easy-rsa/3
+touch vars
+echo "export KEY_COUNTRY=\"EU\"" >> vars
+echo "export KEY_PROVINCE=\"UNKNOW\"" >> vars
+echo "export KEY_CITY=\"UNKNOW\"" >> vars
+echo "export KEY_ORG=\"UNKNOW\"" >> vars
+echo "export KEY_EMAIL=\"UNKNOW@UNKNOW.UNKNOW\"" >> vars
+echo "export KEY_CN=\"UNKNOW\"" >> vars
+echo "export KEY_OU=\"UNKNOW\"" >> vars
+echo "export KEY_NAME=\"UNKNOW\"" >> vars
+echo "export KEY_ALTNAMES=\"UNKNOW\"" >> vars
+
+. ./vars
+./easyrsa init-pki
+./easyrsa build-ca
+./easyrsa gen-dh
+
+clear
+
+if [[ $servercert ]] 
+then 
+./easyrsa gen-req vpn-server
+else
+./easyrsa gen-req vpn-server nopass
+fi
+
+./easyrsa sign-req server vpn-server
+openvpn --genkey --secret pki/ta.key
+
+mkdir /etc/openvpn/keys
+cp -r pki/* /etc/openvpn/keys/
+
+mkdir /var/log/openvpn
+
+systemctl enable openvpn@server
+systemctl start openvpn@server
+
+cd /usr/share/easy-rsa/3
+
+clear
+
+if [[ $clientcert ]] 
+then 
+./easyrsa gen-req client
+else
+./easyrsa gen-req client nopass
+fi
+
+./easyrsa sign-req client client
+mkdir /tmp/keys
+cp pki/issued/client1.crt pki/private/client1.key pki/dh.pem pki/ca.crt pki/ta.key /tmp/keys
+chmod -R a+r /tmp/keys
+
+echo "local $wanip" >> /etc/openvpn/server.conf
+echo "port 443" >> /etc/openvpn/server.conf
+echo "proto udp" >> /etc/openvpn/server.conf
+echo "dev tun" >> /etc/openvpn/server.conf
+echo "ca keys/ca.crt" >> /etc/openvpn/server.conf
+echo "cert keys/issued/vpn-server.crt" >> /etc/openvpn/server.conf
+echo "key keys/private/vpn-server.key" >> /etc/openvpn/server.conf
+echo "dh keys/dh.pem" >> /etc/openvpn/server.conf
+echo "tls-auth keys/ta.key 0" >> /etc/openvpn/server.conf
+echo "server 172.16.10.0 255.255.255.0" >> /etc/openvpn/server.conf
+echo "ifconfig-pool-persist ipp.txt" >> /etc/openvpn/server.conf
+echo "keepalive 10 120" >> /etc/openvpn/server.conf
+echo "max-clients 32" >> /etc/openvpn/server.conf
+echo "client-to-client" >> /etc/openvpn/server.conf
+echo "persist-key" >> /etc/openvpn/server.conf
+echo "persist-tun" >> /etc/openvpn/server.conf
+echo "status /var/log/openvpn/openvpn-status.log" >> /etc/openvpn/server.conf
+echo "log-append /var/log/openvpn/openvpn.log" >> /etc/openvpn/server.conf
+echo "verb 0" >> /etc/openvpn/server.conf
+echo "mute 20" >> /etc/openvpn/server.conf
+echo "daemon" >> /etc/openvpn/server.conf
+echo "mode server" >> /etc/openvpn/server.conf
+echo "tls-server" >> /etc/openvpn/server.conf
+echo "comp-lzo" >> /etc/openvpn/server.conf
+echo "push \"redirect-gateway def1\"" >> /etc/openvpn/server.conf
+echo "push \"dhcp-option DNS 8.8.8.8\"" >> /etc/openvpn/server.conf
+
+echo "listen-address=127.0.0.1,172.16.10.1" >> /etc/dnsmasq.conf 
+echo "bind-interfaces" >> /etc/dnsmasq.conf 
+
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf #Activete Redirect traffic beetwen interfaces after reload
+echo 1 > /proc/sys/net/ipv4/ip_forward #Activete Redirect traffic beetwen interfaces now
+
+chmod +x /etc/rc.local
+chmod 0777 /etc/rc.local
+
+firewall-cmd --direct --permanent --add-rule ipv4 filter FORWARD 0 -i eth0 -o tun0 -j ACCEPT
+firewall-cmd --direct --permanent --add-rule ipv4 filter FORWARD 0 -i tun0 -o eth0 -j ACCEPT
+firewall-cmd --permanent --zone=dmz --add-masquerade
+firewall-cmd --permanent --add-masquerade
+firewall-cmd --permanent --direct --add-rule ipv4 nat POSTROUTING 0 -o eth0 -j MASQUERADE
+firewall-cmd --direct --permanent --add-rule ipv4 filter FORWARD 0 -i eth0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+echo "service dnsmasq start" >> /etc/rc.local
+
+mkdir /var/www/html/keys
+cp -rf /tmp/keys/* /var/www/html/keys
+cd /var/www/html
+tar -cvf keys.tar.gz keys
+cp keys.tar.gz keys/
+systemctl start httpd
+
+clear
+
+echo "Please download you key pack, in browser: http://ip_server/keys/keys.tar.gz"
+echo "After reboot web server will disable"
+echo "Warning: You have ONE minute for download"
+
+secs=$((5 * 12))
+while [ $secs -gt 0 ]; do
+   echo -ne "$secs\033[0K\r"
+   sleep 1
+   : $((secs--))
+done
+
+reboot
