9f8ab7c1ef30bbbe27ac4b3a8eaf0acbe33efd33
MAXPOL <maxrol32@gmail.com>
2023-01-07 22:26:19 +0300

    Update settingServer.sh

--- settingServer.sh
+++ settingServer.sh
@@ -128,7 +128,7 @@ iptables -I INPUT -i eth0 -m state --state NEW -p udp --dport 1194 -j ACCEPT
 iptables -I FORWARD -i tun+ -j ACCEPT  
 iptables -I FORWARD -i tun+ -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT  
 iptables -I FORWARD -i eth0 -o tun+ -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.16.10.0/24 -o eth0 -j MASQUERADE
 iptables -A OUTPUT -o tun+ -j ACCEPT
 
 
